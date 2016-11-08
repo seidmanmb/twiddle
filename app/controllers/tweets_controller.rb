821c8446df338c901034e2fe8882b0b2dd1ea942
Michael B. Seidman <seidmanmb@gmail.com>
2016-11-07 20:34:13 -0600

    Set up new and show tweets views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,38 @@
 class TweetsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet,
+                      notice: 'Nice Tweeting!' }
+      else
+        format.html { render new }
+      end
+    end
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'tweets#index'
+  # tweets controller and the index view
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
